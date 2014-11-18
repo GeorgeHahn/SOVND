7c6335f98ba1a014c9fda9d77e8a6884092e495e
George Hahn <george@genericmaker.com>
2014-11-18 12:09:11 -0500

    Moving bits to their own files

--- SOVND.Server/Program.cs
+++ SOVND.Server/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Charlotte;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -214,42 +213,5 @@ namespace SOVND.Server
         }
 
         private Dictionary<string, Channel> channels = new Dictionary<string, Channel>();
-    }
-
-    public class Channel
-    {
-        public string Name { get; set; }
-
-        public string Description { get; set; }
-
-        public string Image { get; set; }
-
-        // TODO Moderators
-
-        public Dictionary<string, Song> SongsByID { get; set; } = new Dictionary<string, Song>();
-
-        /// <summary>
-        /// Gets the song at the top of the list
-        /// TODO Return an enum that counts down from the top of the list (write a sorting function and use C#'s sorting)
-        /// </summary>
-        /// <returns></returns>
-        public Song GetTopSong()
-        {
-            Song max = SongsByID.Values.FirstOrDefault();
-            foreach (var song in SongsByID.Values)
-            {
-                if (song == max)
-                    continue;
-
-                if (song.Votes > max.Votes)
-                {
-                    max = song;
-                } else if (song.Votes == max.Votes && song.Votetime < max.Votetime)
-                {
-                    max = song;
-                }
-            }
-            return max;
-        }
     }
 }
