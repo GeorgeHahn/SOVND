4a806764162c65d3212b5f9245135b8c29d63c23
George Hahn <george@genericmaker.com>
2014-11-22 14:54:51 -0500

    Add settings viewmodel, closes #29

---
+++ SOVND.Client/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOVND.Client.ViewModels
+{
+    public class SettingsViewModel
+    {
+        public string SOVNDUsername { get; set; }
+        public string SOVNDPassword { get; set; }
+        public string SpotifyUsername { get; set; }
+        public string SpotifyPassword { get; set; }
+        public string LastfmUsername { get; set; }
+        public string LastfmPassword { get; set; }
+    }
+}
