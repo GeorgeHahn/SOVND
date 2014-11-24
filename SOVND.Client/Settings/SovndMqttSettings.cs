27f2efa37a58717e63882649ddd1789097308cc6
George Hahn <george@genericmaker.com>
2014-11-23 19:22:36 -0500

    Read client auth from file, closes #34

--- SOVND.Client/Settings/SovndMqttSettings.cs
+++ SOVND.Client/Settings/SovndMqttSettings.cs
@@ -1,22 +1,30 @@
 ﻿using SOVND.Lib;
+using SOVND.Lib.Settings;
 
 namespace SOVND.Client.Settings
 {
     public class SovndMqttSettings : IMQTTSettings
     {
+        private readonly ISettingsProvider _settings;
+
+        public SovndMqttSettings(ISettingsProvider settings)
+        {
+            _settings = settings;
+        }
+
         public string Broker { get { return "104.131.87.42"; } }
 
-        public int Port { get { return 8883; } }
+        public int Port { get { return 2883; } }
 
 
         public string Username
         {
-            get { return "georgehahn"; }
+            get { return _settings.GetAuthSettings().SOVNDUsername; }
         }
 
         public string Password
         {
-            get { return ""; }
+            get { return _settings.GetAuthSettings().SOVNDPassword; }
         }
     }
 }
