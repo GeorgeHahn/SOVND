d832c56ab3e1bf9655e86729d781d54e896ee628
George Hahn <george@genericmaker.com>
2014-11-30 23:56:21 -0500

    Moving the last bits of logging over to NLog

--- SpotifyClient/Log.cs
+++ SpotifyClient/Log.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Anotar.NLog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,25 +10,54 @@ namespace SpotifyClient
     public class Log
     {
         public static void Trace(object blah, string message)
-        { }
+        {
+            LogTo.Trace(message);
+        }
+
         public static void Trace(object blah, string message, params object[] format)
-        { }
+        {
+            LogTo.Trace(message, format);
+        }
+
         public static void Debug(object blah, string message)
-        { }
+        {
+            LogTo.Debug(message);
+        }
+
         public static void Debug(object blah, string message, params object[] format)
-        { }
+        {
+            LogTo.Debug(message, format);
+        }
+
         public static void Info(object blah, string message)
-        { }
+        {
+            LogTo.Info(message);
+        }
+
         public static void Info(object blah, string message, params object[] format)
-        { }
+        {
+            LogTo.Info(message, format);
+        }
+
         public static void Warning(object blah, string message)
-        { }
+        {
+            LogTo.Warn(message);
+        }
+
         public static void Warning(object blah, string message, params object[] format)
-        { }
+        {
+            LogTo.Warn(message, format);
+        }
+
         public static void Error(object blah, string message)
-        { }
+        {
+            LogTo.Error(message);
+        }
+
         public static void Error(object blah, string message, params object[] format)
-        { }
+        {
+            LogTo.Error(message, format);
+        }
     }
 
     public enum Plugin
