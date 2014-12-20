41aad527e4e30aead5123256a514ad91ab305350
George Hahn <george@genericmaker.com>
2014-12-20 00:05:03 -0500

    Add markup extensions

---
+++ SOVND.Client/Util/Type2Extension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace SOVND.Client.Util
+{
+    public class Type2Extension : System.Windows.Markup.TypeExtension
+    {
+        public Type2Extension()
+        {
+        }
+
+        public Type2Extension(string typeName)
+        {
+            base.TypeName = typeName;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            IXamlTypeResolver typeResolver = (IXamlTypeResolver)serviceProvider.GetService(typeof(IXamlTypeResolver));
+            int sepindex = TypeName.IndexOf('+');
+            if (sepindex < 0)
+                return typeResolver.Resolve(TypeName);
+            else
+            {
+                Type outerType = typeResolver.Resolve(TypeName.Substring(0, sepindex));
+                return outerType.Assembly.GetType(outerType.FullName + "+" + TypeName.Substring(sepindex + 1));
+            }
+        }
+    }
+}
